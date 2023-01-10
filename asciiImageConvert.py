104507170d725fa4c22d00b12f08fe4c9c496f77
anto__dev <antonidwm@gmail.com>
2023-01-10 23:37:21 +0100

    first commit

---
+++ asciiImageConvert.py
@@ -0,0 +1,21 @@
+
+from PIL import Image
+
+ascii_char = '.+-$*#@'
+asciiSeptante = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+with Image.open("David_aramis01minJpeg.jpg" ) as image:
+    image = image.resize((500,500))
+    
+    for y in range(image.height):
+        line = " "
+        for x in range(image.width) :
+            rgb = image.getpixel((x, y))
+            # on fait la moyenne de chaque channel pour faire une nuance de gris
+            grey = sum(rgb) // len(rgb)
+            # on fait un produit en croix pour récupérer l'index
+            index = grey * 69 // 255
+            # on rajoute 2 espaces, car en console la hauteur d'un char est plus grande que sa largeur
+            line += "<span>"+ asciiSeptante[index] + " "+"</span>"
+        print(line)
+        
+ 
---
+++ readme.md
@@ -0,0 +1,5 @@
+Hello 😄 👋
+I'am create a small project in python for transform a image to an ascii image art.
+For this a use Pillow libary to scan image. 
+
+It's the start of the project 🧰👷‍♂️

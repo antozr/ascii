244ec46ae057cd77d68c6c97d95a59039cd16d5c
anto__dev <antonidwm@gmail.com>
2023-01-11 23:06:06 +0100

    Add intro message / brak line after end

--- asciiImageConvert.py
+++ asciiImageConvert.py
@@ -1,21 +1,33 @@
 
 from PIL import Image
+from intro import helloTxt
 
+
+
+helloTxt()
+
+# the ascii character use for the convert 
 ascii_char = '.+-$*#@'
 asciiSeptante = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+
+# the input for create a unique user image convert 
+print('Enter the path of your jpeg, jpg, png with the FULL path ( whith the disk of origne ) \nExemple : C://User/desktop/myImage.png\n')
 pathImage = input('Enter your url ( the full path ):')
-heigthSizeImg = int(input('Choose the height of your image(just  number) : '))
+heigthSizeImg = int(input('Choose the height of your image(just  number !!) : '))
 widthSizeImg = int(input('Choose the width of your image ( just number !!) : '))
+lengthDefinitionAscii = int(input("Choose de definition of your image between 10 and 69 : "))
+
+
 with Image.open(pathImage ) as image:
     
     image = image.resize((heigthSizeImg,widthSizeImg))
     
     for y in range(image.height):
-        line = " "
+        line = " \n"
         for x in range(image.width) :
             rgb = image.getpixel((x, y))
             grey = sum(rgb) // len(rgb)
-            index = grey * 69 // 255
+            index = grey * lengthDefinitionAscii // 255
             line += asciiSeptante[index] + "  "
             # to web apllication 
             # line += "<span>"+ asciiSeptante[index] + " "+"</span>"
---
+++ intro.py
@@ -0,0 +1,4 @@
+
+
+def helloTxt() :
+    print('\n\nHello, you have in the ASCII Art convertiser \nYou put a normal image and you convert this in a png / html file or a txt file. \nFor this project, fun is here ^^.\n\n\n ')

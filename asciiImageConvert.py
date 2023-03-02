ffc2803fde59776ac1d99b5966ea30ecede50525
anto__dev <antonidwm@gmail.com>
2023-03-02 16:43:50 +0100

    CREATE html render

--- asciiImageConvert.py
+++ asciiImageConvert.py
@@ -14,30 +14,43 @@ asciiSeptante = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:
 resInputUser= inputToUser()
 pathImageUser= resInputUser[0]
 arrayAllLine = []
-
+arrayAllLineHtml = []
 with Image.open(pathImageUser ) as image:
     
     image = image.resize((resInputUser[1],resInputUser[2]))
     
     for y in range(image.height):
         line = " "
+        lineHtml = " "
         for x in range(image.width) :
             rgb = image.getpixel((x, y))
             grey = sum(rgb) // len(rgb)
             index = grey * resInputUser[3] // 255
             line += asciiSeptante[index] + "  "
             # to web apllication 
-            # line += "<span>"+ asciiSeptante[index] + " "+"</span>"
+            lineHtml += "<span>"+ asciiSeptante[index] + " "+"</span>"
         
-        print(line)
+       # print(line)
         arrayAllLine.append(line)
-    print(arrayAllLine)
+        arrayAllLineHtml.append(lineHtml)
+    #print(arrayAllLine)
     #print all line in text file
+    
+    print('Tu veux un rendu en fichier HTML / TXT ou les deux ? \n')
+    choixFichier = input('Enter your choice HTML / TXT / BOTH : ')
     with open('imageArt.txt', 'w') as file :
         for arrayLine in arrayAllLine:
             file.write(arrayLine)
             file.write('\n')
+            
+    with open('ascii.html', 'w') as file :
+        file.write('<span style="color:black; font-size:8px; font-family:\'Consolas\',\'BitstreamVeraSansMono\',\'CourierNew\',Courier,monospace; display:inline-block; white-space:pre; "> \n')
         
+        for arrayLine in arrayAllLineHtml:
+            file.write(arrayLine) 
+            file.write('\n') 
+            
+        file.write('</span>')
         
             
         
